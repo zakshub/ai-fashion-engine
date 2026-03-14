aa2a46d6de05ba4ae2172437a467777aea5f5c79
Zuhaib Akram <info@zuhaib.pro>
2026-03-14 20:51:55 +0500

    comitt app

--- app.py
+++ app.py
@@ -1,26 +1,43 @@
 from flask import Flask, request, jsonify
-import random
+from PIL import Image
+import requests
+from io import BytesIO
 
 app = Flask(__name__)
+
+def get_dominant_color(image_url):
+
+    response = requests.get(image_url)
+    img = Image.open(BytesIO(response.content))
+
+    img = img.resize((50,50))
+
+    pixels = list(img.getdata())
+
+    r = sum(p[0] for p in pixels) // len(pixels)
+    g = sum(p[1] for p in pixels) // len(pixels)
+    b = sum(p[2] for p in pixels) // len(pixels)
+
+    return "#{:02x}{:02x}{:02x}".format(r,g,b)
+
 
 @app.route("/analyze", methods=["POST"])
 def analyze():
 
-    colors = [
-        "#1A1A1A",
-        "#F5E9EF",
-        "#2A3D66",
-        "#C9A227"
-    ]
+    data = request.json
+    image_url = data.get("image")
+
+    color = get_dominant_color(image_url)
 
     result = {
-        "primary_color": random.choice(colors),
+        "primary_color": color,
         "background": "#ffffff",
         "font_color": "#000000",
-        "accent": "#C9A227"
+        "accent": color
     }
 
     return jsonify(result)
+
 
 @app.route("/")
 def home():
